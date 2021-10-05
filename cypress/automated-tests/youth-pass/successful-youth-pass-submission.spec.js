0526d739d9c291a9130818d708d822ef4a44fb43
Kris Johnson <krisjohnson@CTD25138KJOHNSON.local>
2021-10-05 10:33:46 -0400

    update variable scoping

--- cypress/automated-tests/youth-pass/successful-youth-pass-submission.spec.js
+++ cypress/automated-tests/youth-pass/successful-youth-pass-submission.spec.js
@@ -1,18 +1,13 @@
 const faker = require('faker');
 
 describe('Youth Pass Successful Submission', () => {
-    const youthPassUrl = Cypress.env('youth_pass_url');
-    const applicantFirstName = faker.name.firstName();
-    const applicantLastName = faker.name.lastName();
-    const applicantEmailAddress = `Automation_Testing_${faker.datatype.number()}@example.com`;
-    const applicantPhoneNumber = faker.phone.phoneNumber();
-    const randomBirthdate = faker.date.between('1996-11-01', '2004-10-31');
-    const applicantBirthdate = `${randomBirthdate.getMonth() + 1}/${randomBirthdate.getDate()}/${randomBirthdate.getFullYear()}`;
-    const applicantStreetAddress = `${faker.datatype.number()} ${faker.address.streetName()} ${faker.address.streetSuffix()}`;
-    const applicantCity = faker.address.city();
     const applicantZipCode = '02114';
     
     it('completes the eligibility checker', () => {
+        const youthPassUrl = Cypress.env('youth_pass_url');
+        const randomBirthdate = faker.date.between('1996-11-01', '2004-10-31');
+        const applicantBirthdate = `${randomBirthdate.getMonth() + 1}/${randomBirthdate.getDate()}/${randomBirthdate.getFullYear()}`;
+    
         cy.visit(youthPassUrl);
         cy.get('#form-section-0 > .form-section-buttons > .form-section-next').click();
         cy.get('#element105').type(applicantZipCode).blur();
@@ -24,6 +19,9 @@ describe('Youth Pass Successful Submission', () => {
     });
 
     it('completes the personal information section', () => {
+        const applicantFirstName = faker.name.firstName();
+        const applicantLastName = faker.name.lastName();
+
         cy.get('#element116_Option_1').click().blur();
         cy.get('#element11').type(applicantFirstName).blur();
         cy.get('#element13').type(applicantLastName).blur();
@@ -31,12 +29,18 @@ describe('Youth Pass Successful Submission', () => {
     });
 
     it('completes the contact information section', () => {
+        const applicantPhoneNumber = faker.phone.phoneNumber();
+        const applicantEmailAddress = `Automation_Testing_${faker.datatype.number()}@example.com`;
+    
         cy.get('#element17').type(applicantPhoneNumber).blur();
         cy.get('#element18').type(applicantEmailAddress).blur();
         cy.get('#form-section-5 > .form-section-buttons > .form-section-next').click();
     });
     
     it('completes the address section', () => {
+        const applicantStreetAddress = `${faker.datatype.number()} ${faker.address.streetName()} ${faker.address.streetSuffix()}`;
+        const applicantCity = faker.address.city();
+    
         cy.get('#element101').type(applicantStreetAddress).blur();
         cy.get('#element103').type(applicantCity).blur();
         cy.get('#element154').type(applicantZipCode).blur();
