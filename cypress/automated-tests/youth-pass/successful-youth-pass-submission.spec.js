90c56a97b5cefef0a4394f6e0489944c0226fecf
Kris Johnson <krisjohnson@mbta.com>
2021-11-16 16:46:48 -0500

    update successful submission script to select card delivery preference

--- cypress/automated-tests/youth-pass/successful-youth-pass-submission.spec.js
+++ cypress/automated-tests/youth-pass/successful-youth-pass-submission.spec.js
@@ -41,6 +41,7 @@ describe('Youth Pass Successful Submission', () => {
         const applicantStreetAddress = `${faker.datatype.number()} ${faker.address.streetName()} ${faker.address.streetSuffix()}`;
         const applicantCity = faker.address.city();
 
+        cy.get('#element242_Option_1').click().blur();
         cy.get('#element101').type(applicantStreetAddress).blur();
         cy.get('#element103').type(applicantCity).blur();
         cy.get('#element154').type(applicantZipCode).blur();
