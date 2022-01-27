4b83ea21ea12ded3d84ac878e8eb776159c1b274
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2022-01-27 16:39:31 -0500

    test: Add a Cypress test for the Senior success path
    Include a submission:
      • with an email address
      • without an email address
    
    Include this test in GitHub Actions runs against preprod, but not
    against prod.

--- cypress/common/birthdate-constants.js
+++ cypress/common/birthdate-constants.js
@@ -9,15 +9,19 @@ export function getRandomApplicantAge() {
     const eighteenYearsAgo = baselineYear - 18; 
     const twentySixYearsAgo = baselineYear - 26; 
     const fiftyYearsAgo = baselineYear - 50;
+    const sixtyFiveYearsAgo = baselineYear - 65;
+    const ninetyYearsAgo = baselineYear - 90;
     const currentDate = `${todaysDate.getMonth() + 1}/${todaysDate.getDate()}/${todaysDate.getFullYear()}`;
     const randomDateTooYoung = faker.date.between(`${twelveYearsAgo}-11-02`, currentDate);
     const randomDateTooOld = faker.date.between(`${fiftyYearsAgo}-01-01`, `${twentySixYearsAgo}-11-01`);
     const randomDate12to17 = faker.date.between(`${eighteenYearsAgo}-11-02`, `${twelveYearsAgo}-11-01`);
     const randomDate18to25 = faker.date.between(`${twentySixYearsAgo}-11-02`, `${eighteenYearsAgo}-11-01`);
+    const randomDate65to90 = faker.date.between(`${ninetyYearsAgo}-11-02`, `${sixtyFiveYearsAgo}-11-01`);
     const applicantBirthdateTooYoung = `${randomDateTooYoung.getMonth() + 1}/${randomDateTooYoung.getDate()}/${randomDateTooYoung.getFullYear()}`;    
-    const applicantBirthdateTooOld = `${randomDateTooOld.getMonth() + 1}/${randomDateTooOld.getDate()}/${randomDateTooOld.getFullYear()}`;    
-    const applicantBirthdate12to17 = `${randomDate12to17.getMonth() + 1}/${randomDate12to17.getDate()}/${randomDate12to17.getFullYear()}`;    
-    const applicantBirthdate18to25 = `${randomDate18to25.getMonth() + 1}/${randomDate18to25.getDate()}/${randomDate18to25.getFullYear()}`;    
+    const applicantBirthdateTooOld = formatDate(randomDateTooOld);
+    const applicantBirthdate12to17 = formatDate(randomDate12to17);
+    const applicantBirthdate18to25 = formatDate(randomDate18to25);
+    const applicantBirthdate65to90 = formatDate(randomDate65to90);
     
     
     /** Return the year in which a Youth Pass program year begins (e.g., 2021-22 program year begins in 2021)
@@ -33,7 +37,20 @@ export function getRandomApplicantAge() {
             programYearStart = todaysDate.getFullYear();        
         }
         return programYearStart;
-    };
+    }
 
-    return {applicantBirthdateTooYoung, applicantBirthdateTooOld, applicantBirthdate12to17, applicantBirthdate18to25}
-}
+    /**
+     * Formats a date as a string in M/D/YYYY format
+     */
+    function formatDate(date) {
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    }
+
+    return {
+        applicantBirthdateTooYoung,
+        applicantBirthdateTooOld,
+        applicantBirthdate12to17,
+        applicantBirthdate18to25,
+        applicantBirthdate65to90
+    };
+};
