983caec291617fa22118c3b0dac80fa4ee340875
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2022-01-28 17:14:52 -0500

    test: Add a Cypress test for the Senior success path (#67)
    Include a submission:
      • with an email address
      • without an email address (also ensures that the application
         can submit successfully without having to upload Photo ID or
         a Rider Photo)
    
    Include this test in GitHub Actions runs against preprod, but not
    against prod.

---
+++ cypress/automated-tests/senior/successful-senior-submission.spec.js
@@ -0,0 +1,212 @@
+import { getRandomApplicantAge } from '../../common/birthdate-constants';
+
+describe('youth pass successful new submission - with an email address', () => {
+  const faker = require('faker');
+  const applicantZipCode = '99999';
+
+  it("loads the senior form", function() {
+    const seniorUrl = Cypress.env('senior_url');
+    cy.visit(seniorUrl);
+  });
+
+  it('completes the language selection', () => {
+    cy
+      .get('[data-field-code="LanguageSelection"]')
+      .contains('English')
+      .click();
+    cy
+      .get('#form-section-0 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("selects apply for a new card", function() {
+    cy
+      .get('[data-field-code="ApplicantStatus"]')
+      .contains('Apply for a Senior CharlieCard')
+      .click();
+    cy
+      .get('#form-section-1 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("clicks through instructions", function() {
+    cy
+      .get('#form-section-2 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("enters personal information", function() {
+    const applicantBirthdate = getRandomApplicantAge().applicantBirthdate65to90;
+    const applicantFirstName = faker.name.firstName();
+    const applicantLastName = faker.name.lastName();
+
+    cy.get('#element10').type(applicantBirthdate).blur();
+    cy.get('#element12').type(applicantFirstName).blur();
+    cy.get('#element13').type(applicantLastName).blur();
+    cy
+      .get('#form-section-3 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("enters an email address and phone number for contact info", function() {
+    const applicantPhoneNumber = faker.phone.phoneNumberFormat();
+    const applicantEmailAddress = `Automation_Testing_${faker.datatype.number()}@example.com`;
+
+    cy.get('#element15').type(applicantPhoneNumber).blur();
+    cy.get('#element16').type(applicantEmailAddress).blur();
+    cy
+      .get('#form-section-4 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("uploads a photo ID", function() {
+    cy
+      .get('#element36')
+      .attachFile('youth-pass-test-image.png');
+    cy.get('.k-text-success').should('exist');
+    cy
+      .get('#form-section-5 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("uploads a headshot", function() {
+    cy
+      .get('#element39')
+      .attachFile('youth-pass-test-image.png');
+    cy.get('.k-text-success').should('exist');
+    cy
+      .get('#form-section-6 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("enters a home address", function() {
+    const applicantStreetAddress = `${faker.datatype.number()} ${faker.address.streetName()} ${faker.address.streetSuffix()}`;
+    const applicantCity = faker.address.city();
+    const applicantZipCode = '99999';
+
+    cy
+      .get('[data-field-code="CardDeliveryPreference"]')
+      .contains('Mail to my address')
+      .click();
+    cy.get('#element22').type(applicantStreetAddress).blur();
+    cy.get('#element24').type(applicantCity).blur();
+    cy.get('#element26').type(applicantZipCode).blur();
+    cy
+      .get('[data-field-code="MailingAddressSameAsHome"]')
+      .contains('Yes')
+      .click();
+    cy
+      .get('#form-section-7 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("skips the demographic section and agrees to the rules", function() {
+    cy
+      .get('#form-section-8 > .form-section-buttons > .form-section-next')
+      .click();
+    cy
+      .get('[data-field-code="RulesAndConditionsCheckbox"]')
+      .contains('I agree')
+      .click();
+    cy
+      .get('#form-element-wrapper_2 > div > .form-submit-button')
+      .click();
+
+    cy
+      .get('#thank-you-text', { timeout: 15000 })
+      .should('contain', 'Application Submitted');
+  });
+});
+
+describe('youth pass successful renewal submission - without an email address', () => {
+  const faker = require('faker');
+  const applicantZipCode = '99999';
+
+  it("loads the senior form", function() {
+    const seniorUrl = Cypress.env('senior_url');
+    cy.visit(seniorUrl);
+  });
+
+  it('completes the language selection', () => {
+    cy
+      .get('[data-field-code="LanguageSelection"]')
+      .contains('English')
+      .click();
+    cy
+      .get('#form-section-0 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("selects renew my card", function() {
+    cy
+      .get('[data-field-code="ApplicantStatus"]')
+      .contains('Renew')
+      .click();
+    cy
+      .get('#form-section-1 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("enters personal information", function() {
+    const applicantBirthdate = getRandomApplicantAge().applicantBirthdate65to90;
+    const applicantFirstName = faker.name.firstName();
+    const applicantLastName = faker.name.lastName();
+
+    cy.get('#element10').type(applicantBirthdate).blur();
+    cy.get('#element12').type(applicantFirstName).blur();
+    cy.get('#element13').type(applicantLastName).blur();
+    cy
+      .get('#form-section-3 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("does not enter an email address or phone number for contact info", function() {
+    cy
+      .get('#form-section-4 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("skips uploading a headshot", function() {
+    cy
+      .get('#form-section-6 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("enters a home address", function() {
+    const applicantStreetAddress = `${faker.datatype.number()} ${faker.address.streetName()} ${faker.address.streetSuffix()}`;
+    const applicantCity = faker.address.city();
+    const applicantZipCode = '99999';
+
+    cy
+      .get('[data-field-code="CardDeliveryPreference"]')
+      .contains('Mail to my address')
+      .click();
+    cy.get('#element22').type(applicantStreetAddress).blur();
+    cy.get('#element24').type(applicantCity).blur();
+    cy.get('#element26').type(applicantZipCode).blur();
+    cy
+      .get('[data-field-code="MailingAddressSameAsHome"]')
+      .contains('Yes')
+      .click();
+    cy
+      .get('#form-section-7 > .form-section-buttons > .form-section-next')
+      .click();
+  });
+
+  it("skips the demographic section and agrees to the rules", function() {
+    cy
+      .get('#form-section-8 > .form-section-buttons > .form-section-next')
+      .click();
+    cy
+      .get('[data-field-code="RulesAndConditionsCheckbox"]')
+      .contains('I agree')
+      .click();
+    cy
+      .get('#form-element-wrapper_2 > div > .form-submit-button')
+      .click();
+
+    cy
+      .get('#thank-you-text', { timeout: 15000 })
+      .should('contain', 'Application Submitted');
+  });
+});
