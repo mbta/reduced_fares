42af10e686d6881b089088314a7d613f256deab3
Kris Johnson <krisjohnson@mbta.com>
2022-03-30 12:30:13 -0400

    remove web security

--- cypress/automated-tests/youth-pass/document-viewer-s3-urls.spec.js
+++ cypress/automated-tests/youth-pass/document-viewer-s3-urls.spec.js
@@ -27,20 +27,22 @@ describe('Youth Pass Document Viewer S3 URL Visibility', () => {
         cy.get('.form-submit-button').click();
         cy.get('#thank-you-text').should('be.visible');
         cy.wait(2000);
-        
-        // view audit trail and assert
-        const applicationActionButton = '#dataGridContainer > .dx-gridbase-container > .dx-datagrid-rowsview > .dx-datagrid-content-fixed > .dx-datagrid-table > tbody > [aria-rowindex="1"] > .actions-column > .select2 > .selection > .select2-selection';
         cy.get('.exit-workflow-text').click();
         cy.wait(2000);
         cy.get('#yesBtn').click();
         cy.wait(2000);
+        
+        // view audit trail and assert
         cy.get('.dashboard > .records').click();
         cy.get('[aria-rowindex="1"]').should('contain', 'Approved (pick up later)')
-        cy.get(applicationActionButton).click();
-        cy.get(':nth-child(4) > .dashboard-actions').should('be.visible').click();
-        cy.get(':nth-child(4) > .wrapper-switcher').click();
-        cy.get('#AuditLogDiv > :nth-child(4)').should('contain', 'ProofOfAgeS3URL')
-        cy.get('#AuditLogDiv > :nth-child(4)').should('contain', 'ProofOfResidencyS3URL')
-        cy.get('#AuditLogDiv > :nth-child(4)').should('contain', 'ProofOfEligibilityS3URL')
+        cy.get('.mytasksboard > .records').click();
+        cy.get('#dx-col-87').click();
+        cy.wait(2000);
+        cy.get('.dx-datagrid-content-fixed > .dx-datagrid-table > tbody > [aria-rowindex="1"] > .actions-column > .edit-task > span').click();
+        cy.get('#show-info-btn').click();
+        cy.get(':nth-child(5) > .wrapper-switcher').click();
+        cy.get('#AuditLogDiv > :nth-child(5)').should('contain', 'ProofOfAgeS3URL')
+        cy.get('#AuditLogDiv > :nth-child(5)').should('contain', 'ProofOfResidencyS3URL')
+        cy.get('#AuditLogDiv > :nth-child(5)').should('contain', 'ProofOfEligibilityS3URL')
     });
 });
