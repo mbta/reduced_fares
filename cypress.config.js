002ba927b4132e3496e50249ae963e686f07dce3
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2022-08-31 10:54:52 -0400

    test: Automated Cypress tests for the TAP form (#206)
    - Successful form submissions with and without an email address
    - Required fields

--- cypress.config.js
+++ cypress.config.js
@@ -15,6 +15,8 @@ module.exports = defineConfig({
       'https://mbta.preprod.simpligov.com/preprod/portal/ShowWorkFlow/AnonymousEmbed/4fd3d3fb-6b6a-4521-b214-896099a22af8',
     blind_url:
       'https://mbta.preprod.simpligov.com/preprod/portal/ShowWorkFlow/AnonymousEmbed/bc44cab3-9a58-4022-bd9f-e91699d681a1',
+    tap_url:
+      'https://mbta.preprod.simpligov.com/preprod/portal/ShowWorkFlow/AnonymousEmbed/933fc655-36f5-449d-9d37-dc9f1906477d',
   },
   e2e: {
     // We've imported your old cypress plugins here.
